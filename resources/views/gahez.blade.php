1377cb50b7d6c1f48fdee22f8d6d898c89778600
Hady-Hassan <90941057+Hady-Hassan@users.noreply.github.com>
2022-01-03 21:24:34 +0200

    laravel edit

--- resources/views/gahez.blade.php
+++ resources/views/gahez.blade.php
@@ -21,7 +21,7 @@
         </style>
     </head>
     <body class="antialiased">
-    
+        
 
             <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
                 <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">

9fbfd77b7471842c6f917bff03d98572d4a831f1
Mk662000 <mk840166@gmail.com>
2022-01-03 23:45:32 +0200

    tables

---
+++ database/seeders/tables.php
@@ -0,0 +1,123 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\tablesM;
+class tables extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        tablesM::create(array(
+
+            'class' => 1,
+
+        ));
+
+        tablesM::create(array(
+
+            'class' => 1,
+
+        ));
+
+        tablesM::create(array(
+
+            'class' => 1,
+
+        ));
+
+        tablesM::create(array(
+
+            'class' => 1,
+
+        ));
+
+
+
+
+
+        tablesM::create(array(
+
+            'class' => 2,
+
+        ));
+
+        tablesM::create(array(
+
+            'class' => 2,
+
+        ));
+
+        tablesM::create(array(
+
+            'class' => 2,
+
+        ));
+
+        tablesM::create(array(
+
+            'class' => 2,
+
+        ));
+
+
+        tablesM::create(array(
+
+            'class' => 3,
+
+        ));
+
+        tablesM::create(array(
+
+            'class' => 3,
+
+        ));
+
+        tablesM::create(array(
+
+            'class' => 3,
+
+        ));
+
+        tablesM::create(array(
+
+            'class' => 3,
+
+        ));
+
+
+
+
+
+        tablesM::create(array(
+
+            'class' => 4,
+
+        ));
+
+        tablesM::create(array(
+
+            'class' => 4,
+
+        ));
+
+        tablesM::create(array(
+
+            'class' => 4,
+
+        ));
+
+        tablesM::create(array(
+
+            'class' => 4,
+
+        ));
+
+    }
+}

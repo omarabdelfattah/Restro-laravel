ac31c784c1f51240deecc0c10d6708e031f216ac
omarabdelfattah <omar.mero200125@gmail.com>
2022-01-05 00:26:49 +0200

    fix

--- app/Http/Controllers/landing.php
+++ app/Http/Controllers/landing.php
@@ -9,9 +9,9 @@ use App\Models\categoriesM;
 class landing extends Controller
 {
     public function index(){
+        
         $food  = foodM::where("isoffer",false)->latest()->take(20)->get();   
-        $categories  = categoriesM::latest()->take(7)->get();   
         $page_title = "Restro Restaurant";
-        return view('pages.landing')->with('page_title',$page_title)->with('food',$food)->with('categories',$categories);
+        return view('pages.landing')->with('page_title',$page_title)->with('food',$food);
     }                                      
 }

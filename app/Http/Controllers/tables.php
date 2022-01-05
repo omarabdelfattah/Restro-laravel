6da66073896314fc27df56b04b2508ad58fb05ba
omarabdelfattah <omar.mero200125@gmail.com>
2022-01-05 03:36:03 +0200

    finish

--- app/Http/Controllers/tables.php
+++ app/Http/Controllers/tables.php
@@ -8,8 +8,18 @@ use App\Models\tablesM;
 class tables extends Controller
 {
     public function index(){
-        $tables  = tablesM::latest()->take(10)->get();   
+        $tables_1  = tablesM::where('class',1)->where('reserved',0)->count();   
+        $tables_2  = tablesM::where('class',2)->where('reserved',0)->count();   
+        $tables_3  = tablesM::where('class',3)->where('reserved',0)->count();   
+        $tables_4  = tablesM::where('class',4)->where('reserved',0)->count();   
         $page_title = "Reservation";
-        return view('pages.tables')->with('page_title',$page_title)->with('tables',$tables);
+        return view('pages.tables')->with('page_title',$page_title)
+                                    ->with('tables_1',$tables_1)
+                                    ->with('tables_2',$tables_2)
+                                    ->with('tables_3',$tables_3)
+                                    ->with('tables_4',$tables_4);
     }  
+    public function reserve_table(Request $request){
+        $tables
+    }
 }
